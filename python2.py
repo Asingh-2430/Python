e911af61faa2e997651e298e27ae8d7be1eff4e7
Avjot Singh <119579554+Asingh-2430@users.noreply.github.com>
2022-12-02 16:50:08 +0530

    Deletion of Occurrences

---
+++ python2.py
@@ -0,0 +1,9 @@
+mylist=['hello','how','are','you','hello','how']
+count=0
+o='hello'and'how'
+for i in range(len(mylist)):
+    if i==o:
+        count+=1
+    else:
+        count==2
+        del mylist[i]  
